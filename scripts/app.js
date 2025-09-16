8ebc59c9fd751a77e0a8bdb95c19e18fc8fe59fb
Panhaseth SUY <2024060suy@aupp.edu.kh>
2025-09-16 22:36:36 +0700

    set up tailwind cli and convert header and footer in css file into tailwind

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,40 @@
 import router from './router.js'
 
-const appEl = document.getElementById('app');
-const yearEl = document.getElementById("year");
-const menuBtn = document.getElementById("menu-btn");
+// Initial render 
+// const appEl = document.getElementById('app');
+// function render() {router(appEl)}
 
-yearEl.textContent = new Date().getFullYear();
-menuBtn.addEventListener("click", () => {
-  if(menuBtn.firstChild.src.includes("menu.png")){
-    menuBtn.firstChild.src = './images/icon/close.png';
-    menuBtn.firstChild.style.height = '15px';
-    document.querySelector('.site-header .dynamic-nav').style.display ='block'
-  } else {
-    menuBtn.firstChild.src = './images/icon/menu.png';
-    menuBtn.firstChild.style.height = '20px';
-    document.querySelector('.site-header .dynamic-nav').style.display =''
-  }
+// window.addEventListener("hashchange", render);
+// window.addEventListener("load", render);
+
+// Header Menu toggle
+const profileBtn = document.getElementById('profile-btn');
+const profileMenu = document.getElementById('profile-menu');
+
+profileBtn.addEventListener('click', () => {
+  profileMenu.classList.toggle('hidden');
 });
 
-document.querySelectorAll('.site-header .dynamic-nav ul li').forEach(item => {item.addEventListener('click', () => {
-  menuBtn.firstChild.src = './images/icon/menu.png';
-  menuBtn.firstChild.style.height = '20px';
-  document.querySelector('.site-header .dynamic-nav').style.display =''
-})})
+// Mobile Menu toggle
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
+
+// Close the menu if clicked outside
+window.addEventListener('click', (e) => {
+  if (!profileBtn.contains(e.target) && !profileMenu.contains(e.target)) {
+    profileMenu.classList.add('hidden');
+  }
+  if (!mobileMenuBtn.contains(e.target) && !mobileMenu.contains(e.target)){
+    mobileMenu.classList.add('hidden');
+  }
+})
+
+// Set current year in footer
+document.getElementById('year').textContent = new Date().getFullYear();
 
 
 
@@ -30,7 +43,3 @@ document.querySelectorAll('.site-header .dynamic-nav ul li').forEach(item => {it
 
 
 
-function render() {router(appEl)}
-
-window.addEventListener("hashchange", render);
-window.addEventListener("load", render);
